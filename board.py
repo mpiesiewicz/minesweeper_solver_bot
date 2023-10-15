1559065c2731d72f8f316ed2930986b7470dbc43
mpiesiewicz <michal.piesiewicz@gmail.com>
2023-10-15 14:04:21 +0200

    chore: refactoring

--- board.py
+++ board.py
@@ -10,6 +10,15 @@ class Board():
         self.num_mines = num_mines
         self.tiles = self.generate_tiles()
     
+    # def find_1_2_pattern(self):
+    #     for tile in self.tiles:
+    #         pass
+
+    # def vertical_iterator(self):
+    #     self.current_row = 0
+    #     self.current_col = 0
+
+
     def generate_tiles(self):
         board = [Tile(index, self.num_columns, self.num_rows) for index in range(0, self.num_columns * self.num_rows)]
         for tile in board:
@@ -17,9 +26,11 @@ class Board():
             tile.calculate_mine_chance(self.num_mines)
         return board
         
+
     def reveal(self, index, value):
         self.tiles[index].value = value
         self.tiles[index].state = 'revealed'
+
 
     def recalculate(self):
         for tile in self.tiles:
@@ -27,32 +38,11 @@ class Board():
             # tile.update_values_if_next_to_a_flag()
             tile.propagate_mine_chance()
     
+
     def get_highest_mine_chances(self) -> List[TileType]:
         # 100% chances exist
         return [tile for tile in self.tiles if tile._mine_chance == 1 and tile.flagged == False]
             
-    # def get_lowest_mine_chance(self) -> List[TileType]:
-    #     # get best chance tile
-    #     best_chance = 1
-    #     best_chance_tiles = list()
-
-    #     for tile in self.tiles:
-
-    #         if (tile.mine_chance == 0 and
-    #             tile.flagged == False and
-    #             tile.state != 'revealed'):
-
-    #             best_chance = tile.mine_chance
-    #             best_chance_tiles.append(tile)
-            
-    #         elif (tile.mine_chance < best_chance and 
-    #             tile.state != 'revealed' and
-    #             tile.flagged == False):
-
-    #             best_chance = tile.mine_chance
-    #             best_chance_tiles = [tile]
-
-    #     return best_chance_tiles
 
     def get_lowest_mine_chance(self) -> List[TileType]:
         # Get tiles with a mine chance of zero
@@ -67,21 +57,22 @@ class Board():
             lowest_chance_tile = next(tile for tile in self.tiles if tile.mine_chance == lowest_chance and not tile.flagged and tile.state != 'revealed')
             return [lowest_chance_tile]
 
+
     def __str__(self) -> str:
         return
     
+
     def __getitem__(self, index):
         return self.tiles[index]
 
+
     def __setitem__(self, index, tile_data):
         value, state, flagged = tile_data
-        # self.tiles[index].value = value
-        # self.tiles[index].state = state
-        # self.tiles[index].flagged = flagged
         if index.startswith("t"):
             index = int(index[1:])
         tile = self.tiles[index]
         tile.value, tile.state, tile.flagged = tile_data
+
 
 if __name__ == '__main__':
     board = Board(10, 10, 15)
